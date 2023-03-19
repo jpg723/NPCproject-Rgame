8baa0480a29e9b9a5a3cbe07e0123eeb1583e216
jpg723 <86431761+jpg723@users.noreply.github.com>
2023-03-19 16:39:45 +0900

    [UPDATE] 당근랜덤출력 구현/게임설명이벤트 추가

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
-    private int randomNum;
     public GameObject Success; //성공 이미지
     public GameObject Failure; //실패 이미지
     public GameObject Rabbit; //토끼 이미지
     private bool success_state; //성공이미지 활성화.비활성화 설정
     private bool failure_state; //실패이미지 활성화.비활성화 설정
     private bool rabbit_state; //토끼이미지 활성화.비활성화 설정
+    public GameObject[] Carrot = new GameObject[12]; //당근이미지
+    private bool carrot_state; //토끼이미지 활성화.비활성화 
+    private int sum = 0; //당근 개수 합계
 
     void Start()
     {
-        //랜덤 숫자 생성
-        randomNum = Random.Range(0, 12);
-        Debug.Log(randomNum);
+        CarrotView();
 
         //성공.실패 이미지 비활성화
         success_state = false;
@@ -28,15 +28,55 @@ public class EventManager : MonoBehaviour
         Rabbit.SetActive(rabbit_state);
     }
 
+    //당근 랜덤 출현
+    void CarrotView()
+    {
+        int i = 0;
+        int carrotStateNum; //당근 활성.비활성화 랜덤 결정 정수
+        carrot_state = false;
+
+        //당근 초기화(비활성화)
+        for (i = 0; i < 12; i++)
+            Carrot[i].SetActive(carrot_state);
+
+        //합계 초기화
+        sum = 0;
+
+        for (i = 0; i < 12; i++)
+        {
+            //당근 랜덤으로 활성.비활성화 결정
+            carrotStateNum = Random.Range(0, 2);
+
+            //carrotStateNum이 1이면 활성화
+            if (carrotStateNum == 1)
+                carrot_state = true;
+
+            //0이면 비활성화
+            else
+                carrot_state = false;
+
+            //당근 활성.비활성화
+            Carrot[i].SetActive(carrot_state);
+            //활성화된 당근 개수
+            sum += carrotStateNum;
+        }
+
+        //콘솔 결과 확인
+        Debug.Log("sum: " +  sum);
+    }
+
+
     //짝수 버튼 클릭 시
     public void EvenButtonClick()
     {
-        if (randomNum % 2 == 0)
+        //당근 개수가 짝수라면
+        if (sum % 2 == 0)
         {
             success_state = true;
             failure_state = false;
         }
 
+        //당근 개수가 홀수라면
         else
         {
             success_state = false;
@@ -57,12 +97,14 @@ public class EventManager : MonoBehaviour
     //홀수 버튼 클릭 시
     public void OddButtonClick()
     {
-        if (randomNum % 2 != 0)
+        //당근 개수가 홀수라면
+        if (sum % 2 != 0)
         {
             success_state = true;
             failure_state = false;
         }
 
+        //당근 개수가 짝수라면
         else
         {
             success_state = false;
@@ -79,39 +121,6 @@ public class EventManager : MonoBehaviour
         //랜덤 숫자 재생성 및 토끼 이미지 출력
         Invoke("Start", 3);
     }
-    /*
-    void CarrotPrint(bool cs)
-    {
-        Carrot1.SetActive(cs);
-        Carrot2.SetActive(cs);
-        Carrot3.SetActive(cs);
-        Carrot4.SetActive(cs);
-        Carrot5.SetActive(cs);
-        Carrot6.SetActive(cs);
-        Carrot7.SetActive(cs);
-        Carrot8.SetActive(cs);
-        Carrot9.SetActive(cs);
-        Carrot10.SetActive(cs);
-        Carrot11.SetActive(cs);
-        Carrot12.SetActive(cs);
-    }
 
-    //랜덤 당근 생성
-    bool CarrotState()
-    {
-        bool c_state; //당근 이미지 활성화.비활성화 상태
-        int carrot_state_num; //당근 활성화.비활성화 설정 
-        carrot_state_num = Random.Range(0, 2); //0 또는 1출력
 
-        if (carrot_state_num == 1)
-        {
-            c_state = true;
-        }
-
-        else
-            c_state = false;
-
-        return c_state;
-    }
-    */
 }
