2658cc6f2f70ec37d0012cec22eb89f316ff71a4
jpg723 <86431761+jpg723@users.noreply.github.com>
2023-07-31 21:48:36 +0900

    [ADD] 레벨 기능 구현

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -9,10 +9,15 @@ public class EventManager : MonoBehaviour
     public GameObject Rabbit; //토끼 이미지
     private bool success_state; //성공이미지 활성화.비활성화 설정
     private bool failure_state; //실패이미지 활성화.비활성화 설정
+
+    private int game_success_count = 0; //게임 성공 카운트
+
     private bool rabbit_state; //토끼이미지 활성화.비활성화 설정
     public GameObject[] Carrot = new GameObject[12]; //당근이미지
     private bool carrot_state; //토끼이미지 활성화.비활성화 
     private int sum = 0; //당근 개수 합계
+    private int game_level = 0; //게임 레벨
+    public GameObject[] level = new GameObject[3]; //레벨이미지
 
     void Start()
     {
@@ -39,7 +44,10 @@ public class EventManager : MonoBehaviour
         for (i = 0; i < 12; i++)
             Carrot[i].SetActive(carrot_state);
 
-        //합계 초기화
+        GameLevel();
+        Debug.Log("성공 개수: " + game_success_count);
+
+        //당근 개수 초기화
         sum = 0;
 
         for (i = 0; i < 12; i++)
@@ -62,7 +70,7 @@ public class EventManager : MonoBehaviour
         }
 
         //콘솔 결과 확인
-        Debug.Log("sum: " +  sum);
+        Debug.Log("당근 개수: " +  sum);
     }
 
 
@@ -74,6 +82,7 @@ public class EventManager : MonoBehaviour
         {
             success_state = true;
             failure_state = false;
+            game_success_count++;
         }
 
         //당근 개수가 홀수라면
@@ -102,6 +111,7 @@ public class EventManager : MonoBehaviour
         {
             success_state = true;
             failure_state = false;
+            game_success_count++;
         }
 
         //당근 개수가 짝수라면
@@ -120,6 +130,44 @@ public class EventManager : MonoBehaviour
         Debug.Log("success_state: " + success_state + " / failure_state: " + failure_state);
         //랜덤 숫자 재생성 및 토끼 이미지 출력
         Invoke("Start", 3);
+    }
+
+    //게임 레벨
+    public void GameLevel()
+    {
+        int i = 0;
+
+        //레벨 이미지 활성화/비활성화
+        for (i = 0; i < 3; i++)
+        {
+            //게임 레벨과 인덱스가 같으면 이미지 활성화
+            if (i == game_level)
+                level[i].SetActive(true);
+            else
+                level[i].SetActive(false);
+        }
+
+
+        game_level++; //게임 레벨 카운트
+        Debug.Log("게임 레벨: " + game_level);
+
+        //게임 종료
+        if (game_level == 4)
+        {
+            GameExit();
+            Debug.Log("게임 종료");
+        }
+
+    }
+
+    //게임 종료
+    public void GameExit()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
     }
 
 
