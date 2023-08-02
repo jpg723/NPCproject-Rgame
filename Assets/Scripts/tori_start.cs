596b7f70ebbdd2ba0886f5a13df1f86ba5fdded4
jpg723 <86431761+jpg723@users.noreply.github.com>
2023-08-02 21:32:29 +0900

    [UPDATE] scene 전환 인덱스 오류 수정

--- Assets/Scripts/tori_start.cs
+++ Assets/Scripts/tori_start.cs
@@ -11,6 +11,7 @@ public class tori_start : MonoBehaviour, IPointerDownHandler
     public GameObject chuchu2;
     public GameObject chuchu3;
     public GameObject tori;
+    public GameObject game_explain;
     public Text ScriptText_dialogue;
     public Text ScriptText_name;
     public string[] dialogue
@@ -21,22 +22,32 @@ public class tori_start : MonoBehaviour, IPointerDownHandler
             "내 밭에 있는 당근이 너무 많아서 몇 개인지 모르겠어..!",
             "그런 거라면 문제없지! 내가 당근의 개수를 다 세주면 되는 거지??",
             "개수까지는 괜찮고, 홀수인지 짝수인지만 알려주면 돼!",
-            "이번 게임은 홀짝 게임이야 잘 알고 있지??"
+            "이번 게임은 홀짝 게임이야 잘 알고 있지??", 
+            ".", 
+            "."
         };
     public int dialogue_count = 0;
 
     public void OnPointerDown(PointerEventData data)
     {
         dialogue_count++;
-        Debug.Log(dialogue_count);
 
         if (dialogue_count == 8)
+        {
+            game_explain.SetActive(true);
+        }
+
+        if (dialogue_count == 9)
+        {
+            dialogue_count = 0;
             SceneManager.LoadScene("tori_Game_Scene");
+        }
 
         ScriptText_dialogue.text = dialogue[dialogue_count];
+        Debug.Log(ScriptText_dialogue.text);
 
         //추추 기본 이미지 활성화
-        if (dialogue_count == 0 || dialogue_count == 3)
+        if (dialogue_count == 3)
         {
             chuchu1.SetActive(true);
             chuchu2.SetActive(false);
